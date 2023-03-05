e1df9782c690f9a88f8478ff603872aaeaea1117
kokodak <kokodakadokok@gmail.com>
2023-03-05 23:13:18 +0900

    feat: 시나리오 로직 생성

--- src/main/java/tecotalk/jdbc/JdbcApplication.java
+++ src/main/java/tecotalk/jdbc/JdbcApplication.java
@@ -8,7 +8,12 @@ public class JdbcApplication {
 
     public static void main(String[] args) throws SQLException {
         JdbcExample jdbcExample = new JdbcExample();
+
+        jdbcExample.insertQuery("1", "kokodak", "hello, world!");
         jdbcExample.selectQuery("1");
-        jdbcExample.selectQuery("10");
+        jdbcExample.updateQuery("1", "i am kokodak");
+        jdbcExample.selectQuery("1");
+        jdbcExample.deleteQuery("1");
+        jdbcExample.selectQuery("1");
     }
 }
